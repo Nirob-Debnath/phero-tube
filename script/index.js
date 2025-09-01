41794075a99062857f0188f3c86bd506f9ec4bf9
unknown <nirobdebnath456@gmail.com>
2025-09-01 23:03:00 +0600

    btn done

--- script/index.js
+++ script/index.js
@@ -1,3 +1,10 @@
+function removeActiveClass() {
+    const activeButtons = document.getElementsByClassName("active");
+    for (let btn of activeButtons) {
+        btn.classList.remove("active");
+    }
+}
+
 function loadCategories() {
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
         .then(res => res.json())
@@ -8,8 +15,8 @@ function displayCategory(categories) {
     const categoryContainer = document.getElementById("category-container");
     for (let cat of categories) {
         const categoryDiv = document.createElement("div");
-        categoryDiv.innerHTML = `<button class="btn btn-sm hover:bg-[#FF1F3D]">${cat.category}</button>`;
-        categoryContainer.append(categoryDiv)
+        categoryDiv.innerHTML = `<button id="btn-${cat.category_id}" onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D]">${cat.category}</button>`;
+        categoryContainer.append(categoryDiv);
     }
 }
 
@@ -18,23 +25,46 @@ loadCategories();
 function loadVideos() {
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
         .then(res => res.json())
-        .then(data => displayVideos(data.videos))
+        .then(data => {
+            removeActiveClass();
+            const clickedButton = document.getElementById("btn-all");
+            clickedButton.classList.add("active");
+            displayVideos(data.videos);
+        })
 }
 
 const displayVideos = (videos) => {
     const videoContainer = document.getElementById("video-container");
+    videoContainer.innerHTML = "";
+    if (videos.length === 0) {
+        videoContainer.innerHTML = `
+        <div class="col-span-full flex flex-col justify-center items-center text-center py-20">
+            <img class="w-[120px]" src="./assets/Icon.png" alt="">
+            <h2 class="text-2xl font-bold">Sorry, No content here!</h2>
+        </div>
+        `;
+    }
     videos.forEach((video) => {
         const videoCard = document.createElement("div");
         videoCard.innerHTML = `
         <div class="card bg-base-100 shadow-sm">
-            <figure>
-                <img src=${video.thumbnail} alt="Shoes" />
+            <figure class="relative">
+                <img class="w-full h-[150px] object-cover" src=${video.thumbnail} alt="Shoes" />
+                <span class="absolute bottom-2 right-2 text-white bg-black px-2 text-sm rounded">${video.others.posted_date}</span>
             </figure>
-            <div class="card-body">
-                <h2 class="card-title">${video.title}</h2>
-                <p>${video.description} </p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+            <div class="flex gap-3 px-0 py-5">
+                <div class="profile">
+                    <div class="avatar">
+                        <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring-2 ring-offset-2">
+                            <img src=${video.authors[0].profile_picture} />
+                        </div>
+                    </div>
+                </div>
+                <div class="intro">
+                    <h2 class="text-sm font-semibold">${video.title}</h2>
+                    <p class="text-sm text-gray-400 flex gap-2">${video.authors[0].profile_name} 
+                    <img class="w-5 h-5" src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000" alt=""></p>
+                    <p class="text-sm text-gray-400">${video.others.views} views</p>
                 </div>
             </div>
         </div>
@@ -43,4 +73,13 @@ const displayVideos = (videos) => {
     });
 }
 
-loadVideos();
+const loadCategoryVideos = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            removeActiveClass();
+            const clickedButton = document.getElementById(`btn-${id}`);
+            clickedButton.classList.add("active");
+            displayVideos(data.category);
+        })
+}
